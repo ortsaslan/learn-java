8e35d64ba83b162c97607100213c86c4dfbbe5f3
Aslan Aliev <ortsaslan@gmail.com>
2023-09-20 10:41:14 +0300

    add: javadoc comments

--- stepik/java-basic-course/HelloWorld.java
+++ stepik/java-basic-course/HelloWorld.java
@@ -1,4 +1,16 @@
+/**
+ * Program that prints <code>Hello World</code>
+ * 
+ * @author Aslan Aliev
+ * @version 0.1
+ */
+
 public class HelloWorld {
+	/**
+	 * Program entry point
+	 * 
+	 * @param args command-line arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
