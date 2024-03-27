61251d86189da4436e97b1dac93f57e376a164db
Aslan Aliev <ortsaslan@cube.local>
2024-03-27 18:30:57 +0300

    init: start kennedy-putten project with chapter01

---
+++ kennedy-putten/src/chapter01/MesozoicEden.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+public class MesozoicEden {
+    public static void main(String[] args) {
+        String name = "Anwar";
+        String position = "Park Manager";
+        String openingHours = "08:00";
+        String closingHours = "20:00";
+
+        System.out.println("My name is " + name + " and I want to be a " + position + " in Mesozoic Eden.");
+        System.out.println("Zoo working hours is " + openingHours + " - " + closingHours);
+    }
+}
